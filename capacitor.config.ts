fce09b58c073ec58bea47461a89502bb80403af4
Ranjith RD <ranjithrd03@gmail.com>
2021-03-16 23:16:02 +0530

    initial

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.laravel.app',
+  appName: 'Laramobile',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
